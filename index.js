76f3055b8b99712a951d13eb65a83f5f990cc998
Philip Samoei <samphilkerich04@gmail.com>
2023-04-03 12:01:19 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,45 +1,74 @@
-function displayCharacter (character){
-    let nameElement =document.createElement("li")
-    nameElement.textContent = character.name
-    nameElement.addEventListener("click",()=> {
-        fetch(`http://localhost:3000/characters/${character.id`})
-        .then(res => res.json())
-        .then((character)=> {
+function thumbs1_gen() {
+    // This line below can be changed to define the range for generating randoms
+    let temp = Math.floor(Math.random() * 1000) * 24 + Math.floor(Math.random() * 10);
+    document.getElementById("thumbs1button").innerHTML= " ❤️(" + temp + ")";
+}
 
-            let card =document.createElement("li")
-            card.className= "card col-2 p-o m-4"
-            card.innerHTML = 
-                <img src ="${character.image)" class="card-img-top" alt="${character.name}">
-                
-        
-                document.querySelector(`#item-list`).innerHTML=""
-                document.querySelector(`#item-list`).append(card)
+function thumbs_onclick() {
+    let temp = document.getElementById("thumbs1button").textContent;
+    temp = parseInt(temp.slice(4, temp.indexOf(")"))) + 1;
+    temp = " ❤️(" + temp + ")"
+    document.getElementById("thumbs1button").innerHTML = temp;
+}
 
-                    const voteBtn=card.querySelector("#vote-btn")
-                    voteBtn.addEventListener("click", () => {
-                       const voteCount = card.querySelector("#vote-count")
-                       character.votes++
-                       voteCount.textContent =character.votes
-                    })
-                }
+thumbs1_gen()
 
+function thumbs2_gen() {
+    // This line below can be changed to define the range for generating randoms
+    let temp = Math.floor(Math.random() * 1000) * 24 + Math.floor(Math.random() * 10);
+    document.getElementById("thumbs2button").innerHTML= " ❤️(" + temp + ")";
+}
 
-                    const backBtn = card.querySelector("#back-btn")
-                    backBtn.addEventListener("click", ()=> {
-                        location.reload()
-                    })
-                })
-            })
+function thumbs2_onclick() {
+    let temp = document.getElementById("thumbs2button").textContent;
+    temp = parseInt(temp.slice(4, temp.indexOf(")"))) + 1;
+    temp = " ❤️(" + temp + ")"
+    document.getElementById("thumbs2button").innerHTML = temp;
+}
 
-            document.querySelector("#item-list").append(nameElement)
-        }
+thumbs2_gen()
 
-        function fetchAnimals(){
-            fetch("http://localhost:3000/characters")
-            .then(res => res.json())
-            .then((characters)=> {
-                characters.forEach((character)=>{
-                    displayCharacter(character)
-                })
-            })
-        }
+function thumbs3_gen() {
+    // This line below can be changed to define the range for generating randoms
+    let temp = Math.floor(Math.random() * 1000) * 24 + Math.floor(Math.random() * 10);
+    document.getElementById("thumbs3button").innerHTML= " ❤️(" + temp + ")";
+}
+
+function thumbs3_onclick() {
+    let temp = document.getElementById("thumbs3button").textContent;
+    temp = parseInt(temp.slice(4, temp.indexOf(")"))) + 1;
+    temp = " ❤️(" + temp + ")"
+    document.getElementById("thumbs3button").innerHTML = temp;
+}
+
+thumbs3_gen(0)
+
+function thumbs4_gen() {
+    // This line below can be changed to define the range for generating randoms
+    let temp = Math.floor(Math.random() * 1000) * 24 + Math.floor(Math.random() * 10);
+    document.getElementById("thumbs4button").innerHTML= " ❤️(" + temp + ")";
+}
+
+function thumbs4_onclick() {
+    let temp = document.getElementById("thumbs4button").textContent;
+    temp = parseInt(temp.slice(4, temp.indexOf(")"))) + 1;
+    temp = " ❤️(" + temp + ")"
+    document.getElementById("thumbs4button").innerHTML = temp;
+}
+
+thumbs4_gen(0)
+
+function thumbs5_gen() {
+    // This line below can be changed to define the range for generating randoms
+    let temp = Math.floor(Math.random() * 1000) * 24 + Math.floor(Math.random() * 10);
+    document.getElementById("thumbs5button").innerHTML= " ❤️(" + temp + ")";
+}
+
+function thumbs5_onclick() {
+    let temp = document.getElementById("thumbs2button").textContent;
+    temp = parseInt(temp.slice(4, temp.indexOf(")"))) + 1;
+    temp = " ❤️(" + temp + ")"
+    document.getElementById("thumbs2button").innerHTML = temp;
+}
+
+thumbs5_gen(0)
